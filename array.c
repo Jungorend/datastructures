86761f76b4174f3326bc1aef48b006e3ca6f2df8
Matthew Weber <webermatthewr@gmail.com>
2020-04-21 15:09:32 -0400

    Add dynamic resize

--- array.c
+++ array.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ * As part of learning data structures, the goal is to implement
+ * a dynamic array that has some conveniences you'd see in a higher level language
+ */
+
 struct Array {
   int *A;
   int size;
@@ -15,24 +20,52 @@ void Display(struct Array arr) {
   }
 }
 
+void checkOverflow(struct Array *arr, int newValues) {
+  // This function checks to see if the newValues worth of elements
+  // will cause it to access un-allocated space, and if so resolves it
+  if(arr->size < (arr->length + newValues)) {
+    int *tmpA;
+    int tmpSize = arr->size;
+    int newlength = arr->length + newValues;
+    while(tmpSize < newlength) {
+      tmpSize = tmpSize * tmpSize;
+    }
+
+    // Copy over to a temporary array and then replace it
+    tmpA = (int *)malloc(tmpSize * sizeof(int));
+    for(int i = 0; i < arr->length; i++) {
+      tmpA[i] = arr->A[i];
+    }
+
+    free(arr->A);
+    arr->A = tmpA;
+    arr->size = tmpSize;
+  }
+}
+
 int main() {
-  struct Array arr;
+  struct Array *arr;
   int n,i;
   printf("Enter size of array: ");
-  scanf("%d", &arr.size);
-  arr.A = (int *) malloc(arr.size * sizeof(int));
-  arr.length = 0;
+  scanf("%d", &arr->size);
+  arr->A = (int *) malloc(arr->size * sizeof(int));
+  arr->length = 0;
 
   printf("Enter how many numbers: ");
   scanf("%d",&n);
+  checkOverflow(arr, n);
+  arr->length = arr->length + n;
 
   printf("Enter all elements: ");
   for(i=0;i<n;i++) {
-    scanf("%d", &arr.A[i]);
+    scanf("%d", &arr->A[i]);
   }
-  arr.length = n;
 
-  Display(arr);
+
+
+  printf("Size of array: %d\nLength of Array: %d\n", arr->size, arr->length);
+
+  Display(*arr);
 
   return 0;
 }
